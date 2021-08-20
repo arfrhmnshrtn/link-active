592881587edc64bdf8090e53de7b31f3f22a0e8b
arfrhmnshrtn <iw8628940@gmail.com>
2021-08-20 21:37:50 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+const parent = document.querySelector('.wrapper');
+const btns = document.querySelectorAll('.btn');
+
+for (let i = 0; i < btns.length; i++){
+    btns[i].addEventListener('click', function(){
+        let current = document.getElementsByClassName('active');
+        current[0].className = current[0].className.replace(' active');
+        this.className += ' active';
+    })
+}
